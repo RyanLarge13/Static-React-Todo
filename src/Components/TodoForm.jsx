f40d27a548b7bacf145fb630bde1cef54cc8e24f
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-09-17 15:52:17 -0700

    Initial commit.

---
+++ src/Components/TodoForm.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const TodoForm = (props) => {
+  const [input, setInput] = useState("");
+
+  const focus = useRef(null);
+
+  useEffect(() => {
+    focus.current.focus();
+  });
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit = {
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+    };
+
+    setInput("");
+  };
+
+  return (
+    <>
+      <form className="todo-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="todo"
+          placeholder="Add todo"
+          name="text"
+          value={input}
+          onChange={handleChange}
+          ref={focus}
+        />
+        <button className="addBtn">Add</button>
+      </form>
+    </>
+  );
+};
+
+export default TodoForm;
---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm.jsx";
+import Todo from "./Todo.jsx";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addToDo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    const newTodo = [todo, ...todos];
+
+    setTodos(newTodo);
+  };
+
+  const updatedTodos = (todoId, newTodo) => {
+    if (!newTodo.text || /^\s*$/.test(newTodo.text)) {
+      return;
+    }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newTodo : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>What are your plans today?</h1>
+      </div>
+      <TodoForm onSubmit={addToDo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updatedTodos={updatedTodos}
+      />
+      */
+    </>
+  );
+};
+
+export default TodoList;
