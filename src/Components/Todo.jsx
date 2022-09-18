1e09b54935625523c369da791958863512291ccb
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-09-17 22:09:19 -0700

    Implimenting Framer Motion.

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import TodoForm from "./TodoForm.jsx";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
+import { Reorder } from 'framer-motion';
 
-const Todo = ({ todos, completeTodo, removeTodo, updatedTodos }) => {
+const Todo = ({ todos, completeTodo, removeTodo, updatedTodos, setAddTodo }) => {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
@@ -22,24 +23,32 @@ const Todo = ({ todos, completeTodo, removeTodo, updatedTodos }) => {
   }
 
   return todos.map((todo, index) => (
-    <div
-      className={todo.isComplete ? "todo-row complete" : "todo-row"}
-      key={index}
+    <Reorder.Group
+      axis="y"
+      values={todo}
+      onReorder={setAddTodo}
     >
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
-      </div>
-      <div className="icons">
-        <RiCloseCircleLine
-          onClick={() => removeTodo(todo.id)}
-          className="delete-icon"
-        />
-        <TiEdit
-          onClick={() => setEdit({ id: todo.id, value: todo.text })}
-          className="edit-icon"
-        />
-      </div>
-    </div>
+      <Reorder.Item
+        whileInView={{scale: [0, 1.1, .8, 1]}}
+        whileHover={{ scale: 1.1}}
+        className={todo.isComplete ? "todo-row complete" : "todo-row"}
+        value={todo}
+      >
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+          {todo.text}
+        </div>
+        <div className="icons">
+          <RiCloseCircleLine
+            onClick={() => removeTodo(todo.id)}
+            className="delete-icon"
+          />
+          <TiEdit
+            onClick={() => setEdit({ id: todo.id, value: todo.text })}
+            className="edit-icon"
+          />
+        </div>
+      </Reorder.Item>
+    </Reorder.Group>
   ));
 };
 
