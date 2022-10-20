f6475c30d7adb5940da7d7b891b9663bd1fbc59c
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-10-20 07:26:30 -0700

    Reading through Reorder/Framermotion docs I realize that for now Reorder.Group will not work on any kind of component. As for now, the implementation of framer motion will stay in the code.

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -2,9 +2,17 @@ import React, { useState } from "react";
 import TodoForm from "./TodoForm.jsx";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
-import { Reorder } from 'framer-motion';
+import { Reorder } from "framer-motion";
 
-const Todo = ({ todos, completeTodo, removeTodo, updatedTodos, setAddTodo }) => {
+const Todo = ({
+  todos,
+  completeTodo,
+  removeTodo,
+  updatedTodos,
+  setAddTodo,
+}) => {
+  const [items, setItems] = useState(todos);
+
   const [edit, setEdit] = useState({
     id: null,
     value: "",
@@ -22,34 +30,33 @@ const Todo = ({ todos, completeTodo, removeTodo, updatedTodos, setAddTodo }) => 
     return <TodoForm edit={edit} onSubmit={submitUpdate} />;
   }
 
-  return todos.map((todo, index) => (
-    <Reorder.Group
-      axis="y"
-      values={todo}
-      onReorder={setAddTodo}
-    >
-      <Reorder.Item
-        whileInView={{scale: [0, 1.1, .8, 1]}}
-        whileHover={{ scale: 1.1}}
-        className={todo.isComplete ? "todo-row complete" : "todo-row"}
-        value={todo}
-      >
-        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-          {todo.text}
-        </div>
-        <div className="icons">
-          <RiCloseCircleLine
-            onClick={() => removeTodo(todo.id)}
-            className="delete-icon"
-          />
-          <TiEdit
-            onClick={() => setEdit({ id: todo.id, value: todo.text })}
-            className="edit-icon"
-          />
-        </div>
-      </Reorder.Item>
+  return (
+    <Reorder.Group axis="y" values={items} onReorder={setItems}>
+      {todos.map((todo, index) => (
+        <Reorder.Item
+          whileInView={{ scale: [0, 1.1, 0.8, 1] }}
+          whileHover={{ scale: 1.1 }}
+          className={todo.isComplete ? "todo-row complete" : "todo-row"}
+          key={index}
+          value={todo}
+        >
+          <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+            {todo.text}
+          </div>
+          <div className="icons">
+            <RiCloseCircleLine
+              onClick={() => removeTodo(todo.id)}
+              className="delete-icon"
+            />
+            <TiEdit
+              onClick={() => setEdit({ id: todo.id, value: todo.text })}
+              className="edit-icon"
+            />
+          </div>
+        </Reorder.Item>
+      ))}
     </Reorder.Group>
-  ));
+  );
 };
 
 export default Todo;
